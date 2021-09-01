4e45a0516952a86b0ed4ce7f980058fe4ce0c8a1
Crowdbotics <team@crowdbotics.com>
2021-09-01 13:34:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "multireligionval_30247Identifier",
+  url: "https://multireligionval_30247Identifier.botics.co",
+  api: "https://multireligionval_30247Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
