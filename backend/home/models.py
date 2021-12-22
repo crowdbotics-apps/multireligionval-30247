1e3bfa08e7445a4c337e293457736825b978bfc0
Crowdbotics <team@crowdbotics.com>
2021-12-22 00:08:37 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,6 +5,26 @@ from django.db import models
 class HomePage(models.Model):
     "Generated Model"
     body = models.TextField()
+    multireligionvalsystem = models.TextField(
+        null=True,
+        blank=True,
+    )
+    multireligionval = models.TextField(
+        null=True,
+        blank=True,
+    )
+    multireligionstodet = models.TextField(
+        null=True,
+        blank=True,
+    )
+    trossamfund = models.TextField(
+        null=True,
+        blank=True,
+    )
+    orgnr = models.IntegerField(
+        null=True,
+        blank=True,
+    )
 
 
 class CustomText(models.Model):
